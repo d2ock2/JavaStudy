6cbfbef291d69363aa05bbfd49bb9b29266d4947
yoondeock <yoondeock@gmail.com>
2020-01-15 16:37:57 +0900

    Goodbye World 메시지 추가

--- src/만들어가면서배우는JAVAplayground/src/HelloWorld.java
+++ src/만들어가면서배우는JAVAplayground/src/HelloWorld.java
@@ -3,9 +3,7 @@ package 만들어가면서배우는JAVAplayground.src;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		
 		System.out.println("Hello World!");
-
+		System.out.println("Goodbye World.");
 	}
-
 }
